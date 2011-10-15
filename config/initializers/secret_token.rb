be63b8a934ef170fab60914678935270b3d84841
Stanislav Versilov <stas.versilov@gmail.com>
2011-10-15 22:19:22 +0400

    the start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExxxtraRu::Application.config.secret_token = 'b96bc97bc6012301e5e7217b242bd9054c62a5ed2faa11d344b0c5407335d768c0986e7d51cf2c55d03911a44486fcc5c776d9a29c8cdb57efaca17ff4f3488e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ExxxtraRu::Application.config.session_store :cookie_store, key: '_exxxtra.ru_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ExxxtraRu::Application.config.session_store :active_record_store
