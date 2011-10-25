f2416f0bf9fe50cc0d86bff97cc4e973061ece35
Stanislav Versilov <stas.versilov@gmail.com>
2011-10-25 23:35:49 +0400

    message form styling and email error handling

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,14 +1,20 @@
 # encoding: utf-8
 
 class SiteController < ApplicationController
-  
+
   def send_message
     puts params[:name]
     puts params[:email]
     puts params[:phone]
     puts params[:message]
     
-    MessageMailer.user_message(params[:name], params[:email], params[:phone], params[:message]).deliver
+    
+    begin
+      MessageMailer.user_message(params[:name], params[:email], params[:phone], params[:message]).deliver
+    rescue
+      print "\n====Email sending error====\n"
+    end
+    
     respond_to do |format|
       format.html { redirect_to '/contacts', :notice => 'Ваше сообщение отправлено, спасибо. Мы ответим вам как можно скорее.' }
     end
