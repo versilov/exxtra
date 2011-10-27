7bd75651036cf756a3a8af2b2e303781906d1036
Stanislav Versilov <stas.versilov@gmail.com>
2011-10-27 17:35:31 +0400

    mail messages to stas.versilov@gmail.com

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -1,11 +1,11 @@
 class MessageMailer < ActionMailer::Base
-  default from: "i@exxxtra.ru"
+  default from: "i@exxtra.ru"
   
   def user_message(name, email, phone, message)
     @name = name
     @email = email
     @phone = phone
     @message = message
-    mail :from => "#{name} <#{email}>", :to => 'i@exxxtra.ru', :subject => "exxxtra.ru: message from #{name}"
+    mail :from => "#{name} <#{email}>", :to => 'stas.versilov@gmail.com', :subject => "exxxtra.ru: message from #{name}"
   end
 end
